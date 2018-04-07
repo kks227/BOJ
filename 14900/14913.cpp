3cf65ac520556b01c78db9ed4135e2c6357821ab
kks227 <wongrikera@nate.com>
2018-04-07 17:07:42 +0900

    등차수열
    폰 수정하려고 했는데 폰에 | 문자 없는거실화?

--- 14900/14913.cpp
+++ 14900/14913.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
 	int a, d, k;
 	scanf("%d %d %d", &a, &d, &k);
-	if((k-a)%d) puts("X");
+	if((k-a)%d || (k-a)/d < 0) puts("X");
 	else printf("%d\n", (k-a)/d+1);
 }
