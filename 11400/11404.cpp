da26f744cb77deef95bacf2dd1b6b7832c302399
Kangsan Kim <kks227@gmail.com>
2018-05-10 13:15:50 +0900

    플로이드
    언제 틀려있던 거야

--- 11400/11404.cpp
+++ 11400/11404.cpp
@@ -1,15 +1,14 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-
-const int INF = 1000000000;
+const int INF = 1e9;
 
 int main(){
 	int N, M, dist[100][100];
 	scanf("%d %d", &N, &M);
 	for(int i=0; i<N; i++)
 		for(int j=0; j<N; j++)
-			dist[i][j] = i==j ? 0 : INF;
+			dist[i][j] = (i==j ? 0 : INF);
 	for(int i=0; i<M; i++){
 		int a, b, c;
 		scanf("%d %d %d", &a, &b, &c);
@@ -21,7 +20,7 @@ int main(){
 				dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
 	for(int i=0; i<N; i++){
 		for(int j=0; j<N; j++)
-			printf("%d ", dist[i][j]);
+			printf("%d ", dist[i][j] < INF ? dist[i][j] : 0);
 		puts("");
 	}
 }
