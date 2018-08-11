75d52e64705261c0fa977b80136d0c44e5b41e32
Kangsan Kim <wongrikera@gmail.com>
2018-08-11 23:38:17 +0900

    회전 초밥
    어이없는 오타가

---
+++ 2500/2531.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int N, D, K, C, A[30000], cnt[3000] = {0}, temp = 0;
+	scanf("%d %d %d %d", &N, &D, &K, &C);
+	C--;
+	for(int i=0; i<N; i++){
+		scanf("%d", A+i);
+		A[i]--;
+		if(i >= N-K){
+			if(cnt[A[i]]++ == 0) temp++;
+		}
+	}
+
+	int result = 0;
+	for(int i=0; i<N; i++){
+		result = max(temp + !cnt[C], result);
+		if(cnt[A[i]]++ == 0) temp++;
+		if(--cnt[A[(i+N-K)%N]] == 0) temp--;
+	}
+	printf("%d\n", result);
+}
