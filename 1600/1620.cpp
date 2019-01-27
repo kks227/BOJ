87bfda6a622a66f977771efc5e0d374480a72428
Kangsan Kim <wongrikera@gmail.com>
2019-01-27 20:03:08 +0900

    왜 틀려있냐
    빡빡하

--- 1600/1620.cpp
+++ 1600/1620.cpp
@@ -1,31 +1,68 @@
-#include <iostream>
-#include <string>
-#include <cctype>
-#include <map>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 using namespace std;
+const int MAX = 100001;
+const int MAX_L = 21;
+
+struct Trie{
+	int go[26], output;
+	Trie();
+	void insert(char *s, int val);
+	void find(char *s);
+};
+
+int tcnt;
+Trie node[MAX*MAX_L];
 
 int main(){
-	string M1[100000];
-	map<string, int> M2;
 	int N, M;
-	cin >> N >> M;
-	for(int i=0; i<N; i++){
-		string name;
-		cin >> name;
-		M1[i] = name;
-		M2[name] = i+1;
+	char S[MAX][MAX_L];
+	Trie& root = node[tcnt++];
+	scanf("%d %d", &N, &M);
+	for(int i = 1; i <= N; ++i){
+		scanf("%s", S[i]);
+		S[i][0] += ('a'-'A');
+		root.insert(S[i], i);
+		S[i][0] -= ('a'-'A');
 	}
-	for(int i=0; i<M; i++){
-		cin.ignore(1);
-		if(isdigit(cin.peek())){
-			int num;
-			cin >> num;
-			cout << M1[num-1] << endl;
+	for(int i = 0; i < M; ++i){
+		char q[MAX_L];
+		scanf("%s", q);
+		if(q[0] <= '9'){
+			int k;
+			sscanf(q, "%d", &k);
+			puts(S[k]);
 		}
 		else{
-			string name;
-			cin >> name;
-			cout << M2[name] << endl;
+			q[0] += ('a'-'A');
+			root.find(q);
 		}
 	}
+}
+
+
+
+Trie::Trie(){
+	fill(go, go+26, -1);
+	output = -1;
+}
+
+void Trie::insert(char *s, int val){
+	if(*s == '\0'){
+		output = val;
+		return;
+	}
+	int k = *s - 'a';
+	if(go[k] < 0) go[k] = tcnt++;
+	node[go[k]].insert(s+1, val);
+}
+
+void Trie::find(char *s){
+	if(*s == '\0'){
+		printf("%d\n", output);
+		return;
+	}
+	int k = *s - 'a';
+	node[go[k]].find(s+1);
 }
--- 7700/7785.cpp
+++ 7700/7785.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <string>
-#include <set>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
 
 int main(){
-
 	int N;
+	unordered_set<string> S;
 	cin >> N;
-	string name, flag;
-	set<string> employee;
-	for(int i=0; i<N; i++){
-		cin >> name >> flag;
-		if(flag == "enter") employee.insert(name);
-		else employee.erase(name);
+	for(int i = 0; i < N; ++i){
+		string name, q;
+		cin >> name >> q;
+		if(q[0] == 'e') S.insert(name);
+		else S.erase(S.find(name));
 	}
-	for(set<string>::reverse_iterator iter=employee.rbegin(); iter!=employee.rend(); iter++)
-		cout << *iter << endl;
-
-    return 0;
+	vector<string> result;
+	for(const string &s: S)
+		result.push_back(s);
+	sort(result.begin(), result.end());
+	for(int i = result.size()-1; i >= 0; --i)
+		cout << result[i] << '\n';
 }
