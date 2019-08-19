4c34a7b61253dc3dc189c4b9f30469242505067b
Kangsan Kim <kks227@gmail.com>
2019-08-19 17:52:17 +0900

    Euler
    클래스화. 보기좋음

--- 1100/1199.cpp
+++ 1100/1199.cpp
@@ -1,57 +1,82 @@
 #include <cstdio>
-#include <set>
 #include <vector>
 using namespace std;
-typedef multiset<int> intSet;
+const int MAX = 1000;
 
-int N, E;
-vector<intSet> adj;
-vector<bool> visited;
+struct Edge{
+    int to, cnt;
+    Edge *dual;
+    Edge(): Edge(-1, 0){}
+    Edge(int to1, int cnt1): to(to1), cnt(cnt1), dual(nullptr){}
+};
 
-void DFS(int current){
-	if(visited[current]) return;
-	visited[current] = true;
-	for(int next: adj[current])
-		DFS(next);
-}
+class Graph{
+public:
+    void input(){
+        scanf("%d", &N);
+        M = 0;
+        for(int i = 0; i < N; ++i){
+            for(int j = 0; j < N; ++j){
+                int val;
+                scanf("%d", &val);
+                if(j > i && val > 0){
+                    Edge *e1 = new Edge(j, val), *e2 = new Edge(i, val);
+                    e1->dual = e2;
+                    e2->dual = e1;
+                    adj[i].push_back(e1);
+                    adj[j].push_back(e2);
+                    degree[i] += val;
+                    degree[j] += val;
+                    M += val;
+                }
+            }
+        }
+    }
 
-void EulerCircuit(int current){
-	while(adj[current].size()){
-		intSet::iterator iter = adj[current].begin();
-		int next = *iter;
-		adj[current].erase(iter);
-		adj[next].erase(adj[next].find(current));
-		EulerCircuit(next);
-	}
-	printf("%d ", current+1);
-}
+    void Eulerian(){
+        for(int i = 0; i < N; ++i){
+            if(degree[i]%2 == 1){
+                puts("-1");
+                return;
+            }
+        }
+
+        EPath.clear();
+        for(int i = 0; i < N; ++i){
+            if(degree[i] > 0){
+                Eulerian(i);
+                break;
+            }
+        }
+
+        if(EPath.size() == M+1){
+            for(int i: EPath)
+                printf("%d ", i+1);
+        }
+        else puts("-1");
+    }
+
+private:
+    int N, M, degree[MAX];
+    vector<Edge*> adj[1000];
+    vector<int> EPath;
+
+    void Eulerian(int curr){
+        for(Edge *e: adj[curr]){
+            if(e->cnt > 0){
+                e->cnt--;
+                e->dual->cnt--;
+                Eulerian(e->to);
+            }
+        }
+        EPath.push_back(curr);
+    }
+};
+
+
 
 int main(){
-	scanf("%d", &N);
-	adj.resize(N);
-	for(int i=0; i<N; i++){
-		for(int j=0; j<N; j++){
-			int n;
-			scanf("%d", &n);
-			if(j<=i) continue;
-			for(int k=0; k<n; k++){
-				adj[i].insert(j);
-				adj[j].insert(i);
-			}
-			E += n;
-		}
-	}
-	E /= 2;
-
-	visited.resize(N);
-	int start;
-	for(start=0; start<N && adj[start].empty(); start++);
-	if(start<N) DFS(start);
-	for(int i=start; i<N; i++){
-		if(!visited[i] && !adj[i].empty() || adj[i].size()%2){
-			puts("-1");
-			return 0;
-		}
-	}
-	if(start<N) EulerCircuit(start);
+    Graph G;
+    G.input();
+    G.Eulerian();
 }
